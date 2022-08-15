a989c50c2397d238fa120f94def6aedcf040f1df
Bortoli94 <gbortoli.rosario@hotmail.com>
2022-08-14 23:38:04 -0300

    Practica2Tarea2SinBonus

--- src/practica_2/Practica2.js
+++ src/practica_2/Practica2.js
@@ -1,10 +1,21 @@
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+
 const site = {
-    pageTitle: 'Unidad 1.2 - Práctica',
-    pageBody: 'Implementación de árbol de componentes y propiedades en React.'
+  pageTitle: "Unidad 1.2 - Práctica",
+  pageBody: "Implementación de árbol de componentes y propiedades en React.",
 };
 
-export default function Practica2 () {
-    return (
-        <div></div>
-    );
+const { pageTitle } = site;
+const { pageBody } = site;
+
+const links = ["https://www.youtube.com/", "https://www.facebook.com/"];
+
+export default function Practica2() {
+  return (
+    <>
+      <Header pageTitle={pageTitle} />
+      <Main pageBody={[pageBody]} />
+    </>
+  );
 }
