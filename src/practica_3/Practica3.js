6da6f81c678098a1700be8f8645a1cece9cd2ee0
MaxiPepa <maximopepa@gmail.com>
2022-08-17 19:24:22 -0300

    Añadidos componentes Input y Button

--- src/practica_3/Practica3.js
+++ src/practica_3/Practica3.js
@@ -1,7 +1,12 @@
-export default function Practica3 () {
-    return (
-        <div>
-            <h3>Práctica 3</h3>
-        </div>
-    );
+import Input from "./components/Input/Input";
+import Button from "./components/Button/Button";
+
+export default function Practica3() {
+  return (
+    <div>
+      <h3>Práctica 3</h3>
+      <Input />
+      <Button />
+    </div>
+  );
 }
