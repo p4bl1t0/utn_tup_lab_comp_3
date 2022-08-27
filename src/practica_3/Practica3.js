279f940230afd81dd76fb0495306ff89b3eb0331
MaxiPepa <maximopepa@gmail.com>
2022-08-27 16:18:20 -0300

    Practica 2 completada

--- src/practica_3/Practica3.js
+++ src/practica_3/Practica3.js
@@ -1,12 +1,10 @@
 import Input from "./components/Input/Input";
-import Button from "./components/Button/Button";
 
 export default function Practica3() {
   return (
     <div>
       <h3>Práctica 3</h3>
       <Input />
-      <Button />
     </div>
   );
 }
