ef1b8463906840aedaa3ece6fa435498bdf2f5ef
MaxiPepa <maximopepa@gmail.com>
2022-09-07 14:17:27 -0300

    Tarea 2 completada

--- src/practica_4/Practica4.js
+++ src/practica_4/Practica4.js
@@ -6,8 +6,11 @@ export default function Practica4() {
   const [checkboxValue, setCheckboxValue] = useState(0);
 
   const inputValueHandler = (event) => {
-    console.log(event.target.value)
     parseInt(event.target.value) === 1 ? setInputValue(1) : setInputValue(0);
+  };
+
+  const checkboxValueHandler = (event) => {
+    event.target.checked === true ? setCheckboxValue(1) : setCheckboxValue(0);
   };
 
   return (
@@ -18,12 +21,18 @@ export default function Practica4() {
       </div>
       <div>
         <label>
-          <input type="checkbox" /> Entrada 2
+          <input type="checkbox" onChange={checkboxValueHandler} /> Entrada 2
         </label>
       </div>
       <div>
         <label for="selectGate">Compuerta lógica:</label>
-        <select id="selectGate"></select>
+        <select id="selectGate">
+          <option value={"or"}>OR</option>
+          <option value={"and"}>AND</option>
+          <option value={"nand"}>NAND</option>
+          <option value={"nor"}>NOR</option>
+          <option value={"xor"}>XOR</option>
+        </select>
       </div>
       <div>
         <span>Salida: {/* aca iria el resultado*/}</span>
