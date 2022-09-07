6fbf41161fa51e1f9cdec0cc7dc0bb6fcc736ffc
MaxiPepa <maximopepa@gmail.com>
2022-09-07 14:11:20 -0300

    Tarea 1 completada

--- src/practica_4/Practica4.js
+++ src/practica_4/Practica4.js
@@ -1,21 +1,33 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
-export default function Practica4 () {
-    const inputRef = useRef();
-    return (
-        <div className="App">
-            <div>
-                <label for="input1">Entrada 1:</label>
-                <input type="number" id="input1" />
-            </div>
-            <div>
-                <label><input type="checkbox" /> Entrada 2</label>
-            </div>
-                <div>
-                <label for="selectGate">Compuerta lógica:</label>
-                <select id="selectGate"></select>
-            </div>
-            <div><span>Salida: { /* aca iria el resultado*/ }</span></div>
-        </div>
-    );
+export default function Practica4() {
+  const inputRef = useRef();
+  const [inputValue, setInputValue] = useState(0);
+  const [checkboxValue, setCheckboxValue] = useState(0);
+
+  const inputValueHandler = (event) => {
+    console.log(event.target.value)
+    parseInt(event.target.value) === 1 ? setInputValue(1) : setInputValue(0);
+  };
+
+  return (
+    <div className="App">
+      <div>
+        <label for="input1">Entrada 1:</label>
+        <input type="number" id="input1" onChange={inputValueHandler} />
+      </div>
+      <div>
+        <label>
+          <input type="checkbox" /> Entrada 2
+        </label>
+      </div>
+      <div>
+        <label for="selectGate">Compuerta lógica:</label>
+        <select id="selectGate"></select>
+      </div>
+      <div>
+        <span>Salida: {/* aca iria el resultado*/}</span>
+      </div>
+    </div>
+  );
 }
