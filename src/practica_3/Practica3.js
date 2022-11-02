97401d88c394f9256eea8f4da82250107d6bf0a4
Lucianosolari <luciano3924@gmail.com>
2022-11-02 08:09:05 -0300

    Practica 3 y 4 terminadas, ver boton borrar de la 3

--- src/practica_3/Practica3.js
+++ src/practica_3/Practica3.js
@@ -52,13 +52,3 @@ export default function Practica3() {
     </div>
   );
 }
-
-/* 
-Crear un `input=text` y un `button`. Agregar un `handler` al `button` que al presionar agregué a un 
-`array`  el valor ingresado en el `input`. Si el campo está vacío no se debería agregar al arreglo. 
-Tras agregar el valor del campo, el mismo debería limpiarse. El `array` debe manejarse como un `state`.
-
-* Renderizar cada item del array en pantalla. Si el valor ingresado "parsea" cómo númerico mostrar el
- valor e indicar como texto si es "par" o "impar" el valor ingresado (redondearlo en caso de ser flotante).
-  Si el valor ingresado es un texto mostrarlo dentro de una caja de texto (`input=text`).
-*/
