ceee6d2396673ce0021b0cc04bbf33ed11c1563f
juani <juangarnero@hotmail.com.ar>
2022-11-04 23:25:25 -0300

    add p5

---
+++ src/MainContent.js
@@ -0,0 +1,24 @@
+import { useContext } from 'react';
+
+import Practica1 from './practica_1/Practica1';
+import Practica2 from './practica_2/Practica2';
+import Practica3 from './practica_3/Practica3';
+import Practica4 from './practica_4/Practica4';
+import pathContext from './practica_5/pathContext';
+
+
+
+export default function MainContent () {
+
+    const {practiceSelected} = useContext(pathContext);
+
+    return (
+        <main>
+
+            { practiceSelected === '#practica_1' && <Practica1 /> }
+            { practiceSelected === '#practica_2' && <Practica2 /> }
+            { practiceSelected === '#practica_3' && <Practica3 /> }
+            { practiceSelected === '#practica_4' && <Practica4 /> }
+        </main>
+    );
+}
