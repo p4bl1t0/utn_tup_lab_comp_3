c2f557ad295c665fe596aae166a28ee941d2636f
LucasButto <lbutto40@gmail.com>
2022-11-05 22:22:35 -0300

    Practicas resueltas

--- src/practica_4/Practica4.js
+++ src/practica_4/Practica4.js
@@ -7,7 +7,7 @@ const Practica4 = () => {
     const [resultLogic,setResultLogic] = useState();
 
     const inputValueHandler = (event) => {
-        setInput1Value(event.target.value == 1 ? 1 : 0)
+        setInput1Value(event.target.value === 1 ? 1 : 0)
     }
     const checkboxHandler = (event) => {
         setInput2Value( event.target.checked ? 1 : 0 )
