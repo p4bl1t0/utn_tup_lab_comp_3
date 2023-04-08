cf276f68c61841c307e77c25845a9ffa04247d02
TournierSebastian <sebastiantournier@gmail.com>
2023-04-08 17:18:15 -0300

    Practica2 Tarea2

--- src/practica_2/Practica2.js
+++ src/practica_2/Practica2.js
@@ -1,5 +1,6 @@
 import Title from "./components/Title";
 import Object from "./components/Object";
+import SendLink from "./components/link";
 const site = {
     pageTitle: 'Unidad 1.2 - Práctica',
     pageBody: 'Implementación de árbol de componentes y propiedades en React.'
@@ -10,11 +11,19 @@ export default function Practica2 () {
         <div>
             <header><Title /></header>
             <main><Object /></main>
-            
+            <nav>
+                <ul>
+                    <li>
+                        <SendLink  url ="https://www7.cuevana3.ch/2809/cars" name= "Cars"/>
+                    </li>
+                    <li>
+                        <SendLink url ="https://www.cuevanahd.net/pelicula/cars-2-una-nueva-aventura-sobre-ruedas/" name = "Cars2"/>
+                    </li>
+                </ul>
+            </nav>
             
         </div>
     );
 }
 export const object1 = site.pageTitle 
-
 export const object2 = site.pageBody
---
+++ src/practica_2/components/link.js
@@ -0,0 +1,4 @@
+const SendLink = (props) => (<> <a href={props.url} target="_blank">{props.name}</a> </>);
+
+
+export default SendLink
