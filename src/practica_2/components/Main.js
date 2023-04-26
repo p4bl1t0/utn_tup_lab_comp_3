41611272b969a5e215f8e17435906ae67f2349fc
GasparZ21 <gasparoneto18@gmail.com>
2023-04-26 20:13:22 -0300

    atributo para abrir en nueva pestaña

--- src/practica_2/components/Main.js
+++ src/practica_2/components/Main.js
@@ -4,7 +4,7 @@ const Main = ({pageBody}) =>{
     return <div><Body pageBody={pageBody}></Body>
                 <p>Mi primer contacto con React!</p>
                 <ul>
-                    <li><a href="https://www.youtube.com/">Youtube</a></li>
+                    <li><a href="https://www.youtube.com/" target="blank">Youtube</a></li>
                     <li><a href="https://www.github.com/">Github</a></li>
                 </ul>
             </div>
