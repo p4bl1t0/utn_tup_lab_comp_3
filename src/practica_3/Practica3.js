dedaecbec3739efff8f25b8d10df523c634bd941
MarceloFracassi <fracassimarcelo@gmail.com>
2023-05-03 20:23:31 -0300

    practica3

--- src/practica_3/Practica3.js
+++ src/practica_3/Practica3.js
@@ -1,73 +1,81 @@
-import React, { useState } from 'react';
-export default function Practica3 () {
-//definir un array como state
-const [Array, setArray]=useState([]);
-//Agregar input
-const [ inputValue, setInputValue] = useState('');
+import React, { useState } from "react";
+export default function Practica3() {
+  //definir un array como state
+  const [Array, setArray] = useState([]);
+  //Agregar input
+  const [inputValue, setInputValue] = useState("");
+  //editar
+  const [validInput, setValidInput] = useState(false);
 
-const InputChangeHandler = (event) => {
-    setInputValue(event.target.value);
-}
-const ButtonClickHandler = (event) => {
-    if (inputValue !== ' '){
-        const _arr = [...Array];
-        _arr.push(inputValue);
-        setArray(_arr)
-        setInputValue(' ')//Limpia input
+  const InputChangeHandler = (e) => {
+    const value1 = (e.target.value1);
+    setInputValue(e.target.value1);
+    setValidInput(value1 !=='');
+  };
+  const ButtonClickHandler = () => {
+    if (inputValue !== " ") {
+      const _arr = [...Array];
+      _arr.push(inputValue);
+      setArray(_arr);
+      setInputValue(" "); //Limpia input
     } else {
-        alert("Debe rellenar el casillero")
+      alert("Debe rellenar el casillero");
     }
-}
-    return (
-        <div>
-            <h3>Práctica 3</h3>
-            <input 
-                value={inputValue} 
-                onChange={InputChangeHandler}
-                type="text"
-            />
-            <button onClick={ButtonClickHandler}>
-                Agregar
-                </button>
+  };
+  const ButtonClickHandler1 = (index) => {
+    const deleteArr = [...Array];
+    deleteArr.splice(index, 1);
+    setArray(deleteArr);
+  };
 
-            {Array.map((value, index) => {
-            return (
-            <div key={index}>
-                {typeof Number(value) === 'number' && !isNaN(value) ? (
-                    <div>
-                        {Math.round(value) % 2 === 0 ? 'Par' : 'Impar'}
-                    </div>
-                ) : (
-                <input type="text" value={value} readOnly />
-                )}
-            </div>
-            );
-            })}
-            </div>
-)
+const editItem= (index) =>{
+    const array2=[...Array];
+
+    if (isNaN(array2[index])) {
+        if (validInput) {
+            array2[index] = inputValue;
+            setArray(array2);
+            setInputValue("");
+            setValidInput(false);
+          } else {
+            alert("No hay valor editado");
+          }
+  }
 }
 
+  return (
+    <div>
+      <h3>Práctica 3</h3>
+      <input value={inputValue} onChange={InputChangeHandler} type="text" />
+      <button onClick={ButtonClickHandler}>Agregar</button>
 
-//Crear un `input=text` y un `button`. Agregar un `handler` al `button` que 
-//al presionar agregué a un `array`  el valor ingresado en el `input`. 
-//Si el campo está vacío no se debería agregar al arreglo. 
-//Tras agregar el valor del campo, el mismo debería limpiarse. 
-//El `array` debe manejarse como un `state`.
+      {Array.map((value, index) => {  //Mapeo del arreglo
+        return (
+          <div key={index}>
+            {typeof Number(value) === "number" && !isNaN(value) ? (
+              <div>{Math.round(value) % 2 === 0 ? "Par" : "Impar"}</div> //el math round redondea el flotante.
+            ) : (
+              <input type="text" value={value} readOnly />
+            )}
+            <button onClick={ButtonClickHandler1.bind(null, index)}>
+              Borrar
+            </button>
+            <button onClick={()=>{editItem(index)}}>Editar</button>
 
-//definir un array como state
-//Agregar input
-//agrego boton 
-// Leer valor del input y meterlo en un state
-//declaro handler del boton (onClick)
-//Leer el valor del input
-//verificar que no este vacio 
-//agrego el valor al array 
-//limpio el input
+          </div>
+        );
+      })}
+    </div>
+  );
+}
 
-//muestro el arreglo state en pantalla (map)
-//verfico el tipo
-//muestro segun corresponda
 /*
-sd
-asd
+
+### Tarea 2: 
+
+* Para cada uno de los items del `array` anterior renderizar dentro de cada fila del listado renderizar un `button` 
+etiquetado como "borrar" de manera que al presionar dicho botón se elimine el elemento del arreglo.
+
+* Agregar un `button` junto al campo de texto que se está renderizando cuando el valor es un texto en el listado y etiquetarlo como "editar". 
+Al presionarlo debe cambiar el valor del elemento de esa línea en el `array` 
 */
