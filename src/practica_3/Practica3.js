e532d7297e9de8af7dcc12469b75c77eb7cbbbec
Santy188 <tisanberon@gmail.com>
2023-07-10 20:58:24 -0300

    Practica 3 completada

--- src/practica_3/Practica3.js
+++ src/practica_3/Practica3.js
@@ -1,45 +1,10 @@
-import React, { useState } from 'react';
-import ArrayRender from './components/ArrayRender';
+import Main from "./components/Main";
 
-export default function Practica3() {
-  const [array, setArray] = useState([]);
-  const [textValue, setTextValue] = useState('');
-
-  const clickHandler = (event) => {
-    event.preventDefault();
-    if (textValue !== '') {
-      const value = parseFloat(textValue);
-      const item = isNaN(value) ? textValue : value;
-      setArray([...array, item]);
-      setTextValue('');
-      console.log(array);
-    } else {
-      alert('Ingrese un valor válido');
-    }
-  };
-
-  const changeTextValue = (event) => {
-    setTextValue(event.target.value);
-  };
-  const deleteHandler = (index) => {
-    const arr = [...array];
-    arr.splice(index, 1);
-    setArray(arr);
-  };
-  return (
-    <div>
-      <h3>Práctica 3</h3>
-      <label>
-        Ingrese algo:
-        <input value={textValue} onChange={changeTextValue} type="text" />
-      </label>
-      <button onClick={clickHandler}>Enviar</button>
-      {array.map((item, index) => (
-        <div key={index}>
-          <ArrayRender item={item} />
-          <button onClick={(deleteHandler)}>Borrar</button>
+export default function Practica3 () {
+    return (
+        <div>
+            <h3>Práctica 3</h3>
+            <Main/>
         </div>
-      ))}
-    </div>
-  );
+    );
 }
